13a24d3a13bfbff45b6c79a2dab98db4b0fa10cf
broly <jpegrande@gmail.com>
2024-09-14 22:28:43 -0400

    paru is broken lets go back to yay and auto set papiros breeze folder color

--- set_environment.sh
+++ set_environment.sh
@@ -14,7 +14,7 @@ check_for_internet() {
 check_for_internet "$@"
 
 # Install the missing packages if we don't have them
-arch_packages=("bluez" "bluez-utils" "git" "less" "base-devel" "dosfstools" "rust" "firefox" "papirus-icon-theme" "spectacle" "gwenview" "kdeconnect" "kcalc" " packagekit-qt6" "flatpak" "gnome-disk-utility" "qbittorrent" "gimp")
+arch_packages=("bluez" "bluez-utils" "git" "less" "base-devel" "dosfstools" "rust" "firefox" "papirus-icon-theme" "spectacle" "gwenview" "kdeconnect" "kcalc" "packagekit-qt6" "flatpak" "gnome-disk-utility" "qbittorrent" "gimp")
 echo "Installing pacman pkgs: "${arch_packages[@]}""
 
 if [[ -f /etc/arch-release ]]; then
@@ -30,28 +30,37 @@ else
 	exit 1
 fi
 
-# Install paru (AUR helper)
-if ! sudo pacman -Q paru >/dev/null 2>&1; then
-	echo "paru is not installed. Installing..."
-	git clone https://aur.archlinux.org/paru.git
-	cd paru || exit
-	makepkg -si
-	cd ../
-	rm -rf paru/
+# Install yay (AUR helper)
+yay_dir="yay"
+if ! sudo pacman -Q yay >/dev/null 2>&1; then
+    echo "yay is not installed. Installing..."
+    if [ -d "$yay_dir" ]; then
+        echo "Removing existing $yay_dir directory..."
+        rm -rf "$yay_dir"
+    fi
+    echo "Cloning yay repository..."
+    git clone https://aur.archlinux.org/yay.git "$yay_dir"
+    cd "$yay_dir" || exit
+    echo "Building and installing yay..."
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf "$yay_dir"
 else
-	echo "paru is already installed."
+    echo "yay is already installed."
 fi
 
 # Install aur packages
-paru_packages=("vscodium-bin")
-echo "Installing aur pkgs: "${paru_packages[@]}""
-for package in "${paru_packages[@]}"; do
-	if ! paru -Q "$package" >/dev/null 2>&1; then
-		paru -S --noconfirm --needed "$package"
+yay_packages=("vscodium-bin" "papirus-folders")
+echo "Installing aur pkgs: "${yay_packages[@]}""
+for package in "${yay_packages[@]}"; do
+	if ! yay -Q "$package" >/dev/null 2>&1; then
+		yay -S --noconfirm --needed "$package"
 	else
 		echo "$package is already installed."
 	fi
 done
+
+papirus-folders -C breeze --theme Papirus-Dark
 
 # Enable Bluetooth
 if [ -f /etc/bluetooth/main.conf ]; then
